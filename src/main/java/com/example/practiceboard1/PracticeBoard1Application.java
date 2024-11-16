d285a61e4f3ed1dffe179c673475b26cf3f968f8
hjh_home <ghkdwjdgus99@naver.com>
2024-11-16 14:44:40 +0900

    [1] 기본 설정

---
+++ src/main/java/com/example/practiceboard1/PracticeBoard1Application.java
@@ -0,0 +1,13 @@
+package com.example.practiceboard1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeBoard1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeBoard1Application.class, args);
+    }
+
+}
